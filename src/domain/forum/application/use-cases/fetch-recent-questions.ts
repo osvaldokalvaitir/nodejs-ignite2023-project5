dc6e8d32ded798f47d4ed75d911cee668ae02ce4
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2026-01-04 12:33:43 -0300

    Listando perguntas recentes

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,6 +1,7 @@
+import { Injectable } from '@nestjs/common'
 import { Question } from '@/domain/forum/enterprise/entities/question'
-import type { QuestionsRepository } from '../repositories/questions-repository'
 import { type Either, right } from '@/core/either'
+import type { QuestionsRepository } from '../repositories/questions-repository'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -13,6 +14,7 @@ type FetchRecentQuestionsUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
