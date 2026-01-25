42ac9c9cc6f6b573210f103344231a56b9dcdd76
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2026-01-25 12:06:05 -0300

    Controller: Deletar Comentário da Pergunta

--- src/domain/forum/application/use-cases/delete-question-comment.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common'
 import { type Either, left, right } from '@/core/either'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
@@ -13,6 +14,7 @@ type DeleteQuestionCommentUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteQuestionCommentUseCase {
   constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
 
