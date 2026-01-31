daa9266b8b6b0065811ae1e91e0f93f371b5149a
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2026-01-31 18:36:15 -0300

    Integração com Cloudflare R2

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -13,7 +13,6 @@ import { EnvModule } from './env/env.module'
       load: [
         () => ({
           DATABASE_URL: process.env.DATABASE_URL,
-          DATABASE_SCHEMA: process.env.DATABASE_SCHEMA,
           JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
           JWT_PUBLIC_KEY: process.env.JWT_PUBLIC_KEY,
           PORT: process.env.PORT,
