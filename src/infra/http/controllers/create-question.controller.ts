0a04267908fb6335f8a324db2e2ced7be73f4ecb
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2026-02-12 22:38:46 -0300

    Respostas com anexos

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -8,7 +8,7 @@ import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 const createQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
-  attachments: z.array(z.uuid()),
+  attachments: z.array(z.uuid()).default([]),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
